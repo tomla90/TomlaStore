2ac4d19f714348aa23758638666e5c2356a6a3fc
Tom André Granberg <tom.andre.granberg@gmail.com>
2023-08-20 21:34:49 +0200

    End of section 3

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,16 +1,19 @@
-import { List } from '@mui/material';
+import { Grid, List } from '@mui/material';
 import { Product } from '../../app/models/product';
 import ProductCard from './ProductCard';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 interface Props {
   products: Product[];
 }
 
 export default function ProductList({ products }: Props) {
   return (
-    <List>
+    <Grid container spacing={4}>
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <Grid item xs={6} md={3} key={product.id}>
+          <ProductCard  product={product} />
+        </Grid>
       ))}
-    </List>
+    </Grid>
   );
 }
